3e5cbaecf1c2230cbc703ce0f7f90476de9e0f39
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-10-18 08:05:03 -0400

    Feat: Adiciona o desafio 23, para inserir os valores combo e tasty em tags foi utilizado o  para adicionar um novo valor ao array e  para adicionar multiplos valores e  para ordenar - REQ23

---
+++ challenges/desafio23.js
@@ -0,0 +1,10 @@
+db.produtos.updateMany({}, { $push:
+    {
+      tags: {
+        $each: ["combo", "tasty"],
+        $sort: 1,
+      },
+    },
+  });
+
+db.produtos.find({}, { nome: true, _id: false, tags: true }).pretty();
